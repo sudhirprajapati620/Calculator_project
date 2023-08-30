9df783567ee57c845587704b8e97f99ebcd7f6dc
sudhirprajapati620 <137153428+sudhirprajapati620@users.noreply.github.com>
2023-08-30 14:17:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
